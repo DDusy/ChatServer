0a2d8a46509bef9fb08f6bcc1fbd7d5b645f3137
DDusy <dkdnb50@gmail.com>
2021-11-16 00:04:40 +0900

    conect
    소켓연결 TCP

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -1,7 +1,7 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
-
+#include <mutex>
 #include <winsock2.h> // window만의 소켓
 #include <MSWSock.h>
 #include <WS2tcpip.h>
@@ -10,6 +10,8 @@
 
 int main()
 {
+
+	// 관련 정보가 wsaData에 채워짐
 	WSAData wsaData;
 
 	/*
@@ -21,16 +23,15 @@ int main()
 	*/
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
+
 	/*
-	* 어떤 주소체계를 가질지
-	* 소켓을 어떻게 사용지  -> tcp or udp?
-	//  tcp는 패킷 유실시 재전송과 오염 체크가 가능하므로 자주사용 udp 는 패킷을 빨리처리
-	* 어떤 프로토콜
-	* return : descriptor // 정수를 받아 사용 실제 리소스를 가르키는 모양
+	* 
+	* 1. ad(Address Family) : 어떤 주소체계를 가질지  ->(AF_INET = IPv4, AF_INET6 = IPv6)
+	* 2. type : 소켓을 어떻게 사용지  ->  TCP(SOCK_STREAM) or UDP(SOCK_DGRAM)?
+	* 3. protocol: 어떤 프로토콜
+	* 4. return : descriptor 정수를 받아 사용 실제 리소스를 가르키는 모양
 	* 
 	* cmd 로  ipconfig로 네트워크 주소 검색
-	* IPv4 주소  AF_INET  . . . . . . . . . : 172.20.10.6
-	* IPv6 주소  AF_INET6 
 	*/
 	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);  //IPv4
 	if (clientSocket == INVALID_SOCKET)	// 소켓 처리 실패시
@@ -52,7 +53,11 @@ int main()
 	// low [0x78][0x56][0x34][0x12] high < little 인텔은 little 
 	// low [0x12][0x34][0x56][0x78] high < big
 
-
+	/*
+	connect 원격 호스트(원격 컴퓨터)와 연결하는 함수입니다. 
+	연결된 정보는 remote_host에 저장됩니다.
+	성공시 0, 오류시 -1을 반환합니다.
+	*/
 	//				소켓			,	소켓 어드레스		  , 주소사이즈
 	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
 	{
@@ -65,7 +70,45 @@ int main()
 
 	while (true)
 	{
-		//서버내용
+		// TODO
+		char sendBuffer[100] = "Hello World!";
+
+		for (int32 i = 0; i < 10; i++)
+		{
+			//클라이언트는 send 서버에선 receive
+			//buffer를 소켓 파일 디스크립터인 소켓으로 전송합니다.
+			//보낸 바이트수를 반환하며 실패시 - 1을 반환합니다.
+			int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+			if (resultCode == SOCKET_ERROR)
+			{
+				int32 errCode = ::WSAGetLastError();
+				cout << "Send ErrorCode : " << errCode << endl;
+				return 0;
+			}
+		}
+
+
+		cout << "Send Data! Len = " << sizeof(sendBuffer) << endl;
+		/*
+		* 소켓 입출력 버퍼
+		* 클라 - 서버 (버퍼를 통해 통신)
+		* 커널 레벨에서 소켓과 관련한 버퍼 생성
+		* 
+		*/
+		char recvBuffer[1000];
+
+		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+		if (recvLen <= 0)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Recv ErrorCode : " << errCode << endl;
+			return 0;
+		}
+
+		cout << "Recv Data! Data = " << recvBuffer << endl;
+		cout << "Recv Data! Len = " << recvLen << endl;
+
+		this_thread::sleep_for(1s);
 	}
 
 	// 소켓 다 사용시 정리
--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -1,120 +1,155 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
-
 #include <atomic>
+#include <mutex>
+#include <windows.h>
 #include <future>
 
-#include <thread>
-#include <string>
-#include <windows.h>
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 #include "ConCurrentQueue.h"
 #include "ConCurrentStack.h"
 
-void HelloThread()
-{
-	cout << "Hello Thread" << endl;
-}
+/*
 
-int sum = 0;
+6. send(int fd, void* buffer, size_t n, int flags)
 
-void Add()
-{
-	for (int32 i = 0; i < 100'00000; ++i)
-		sum++;
-}
-
-void Sub()
-{
-	for (int32 i = 0; i < 100'00000; ++i)
-		sum--;
-}
-//? 무슨 템플릿?
-void HelloThread2(int32 _str) // 가변인자 이용해서 사용
-{
-	//Functor나 람다도 가능
-
-	cout << _str << endl;
-}
-
-//TLS 사용 법
-// c++ 이전 방식 __declspec(thread) int32 value;
-thread_local int32 LThreadId;
-
-
-void ThreadMain(int32 _threadID)
-{
-	LThreadId = _threadID;
-
-	while (true)
-	{
-		cout << "Hi I'm Thread" << LThreadId << endl;
-		this_thread::sleep_for(1s);
-	}
-}
-
-
-LcokQueue<int32> q;
-LockStack<int32> s;
-
-void Push()
-{
-	while (true)
-	{
-		int32 value = rand() % 100;
-		q.Push(value);
-
-		this_thread::sleep_for(10ms);
-	}
-}
-
-void pop()
-{
-	while (true)
-	{
-		int32 data = 0;
-	if (q.TryPop(OUT data))
-		cout << data << endl;
-		
-	}
-}
-
+7. recv(int fd, void* buffer, size_t n, int flags)
+send함수와 사용법이 거의 비슷.
+n바이트를 buffer로 읽습니다. 
+성공시 받은 바이트수를 반환하며 실패시 -1을 반환합니다.
+ */
 int main()
 {
+	//	Bind -> lisen ->accept(socket 반환)->
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
 
-
-	//정적 Lib의 경우 디버깅도 가능
-
-	// cout 의 경우 System Call(OS 커널에 요청)
-	// Window / unix에 따라 다름
-	// 한군데에 종속적이면 옮길때 힘듬
-	// C++11 부터 스레드생성이 생기며 환경이 상관없어짐 
-	// 한국은 windows Server이용 c++ iocp 서버 사용 / 해외는 Unix 거의 사용
-
-
-	/*  TLS 스레드 인덱싱 테스트
-	 vector<std::thread> Thread_Vec;
-
-
-	for (int32 i = 0; i < 10; ++i)
+	/*소켓을 만드는데 바로 이 함수를 사용합니다.
+	소켓 역시 파일로 다루어지기 때문에 반환값은 파일디스크립터입니다.
+	만약 소켓을 여는데 실패했다면 - 1을 리턴합니다.*/
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
 	{
-		int32 threadID = i + 1;
-		Thread_Vec.push_back(thread(ThreadMain, threadID));
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
 	}
+
+	// 나의 주소
+	SOCKADDR_IN serverAddr; // IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY); // 자동으로 선택
+	/*
+	* 네트워크가 어려개일때 유동적으로 동작
 	*/
-	
-	
-	//Lock 이용한 스택 큐 사용
-	thread t1(Push);
-	thread t2(pop);
-	thread t3(pop);
 
-	t1.join();
-	t2.join();
-	t3.join();
+	serverAddr.sin_port = ::htons(7777); // 80 : HTTP
+
+	/*소켓을 바인딩합니다. 지금 넘겨지는 소켓과 이 프로세스와 묶는다(bind)라고 생각하시면 됩니다. 
+	그래서 해당 프로세스는 소켓을 통해 다른 컴퓨터로부터 연결을 받아들일 수 있습니다.*/
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Bind ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// 처리시작
+	/* 소켓을 통해 들어오는 연결을 듣습니다.
+	backlog_queue_size만큼 연결 요청을 큐에 넣습니다. 
+	성공시 0, 오류시 -1을 반환합니다.*/
+	if (::listen(listenSocket, 10) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Listen ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// -----------------------------
+
+	while (true)
+	{
+		SOCKADDR_IN clientAddr; // IPv4
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrLen = sizeof(clientAddr);
+
+		/*
+		어떤 컴퓨터에서 이 컴퓨터로 연결할때 연결을 받아들입니다.
+		연결된 원격 컴퓨터의 정보는 remote_host에 저장됩니다. 오류시에 -1을 반환합니다.
+		*/
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Accept ErrorCode : " << errCode << endl;
+			return 0;
+		}
+
+		// client 연결
+		char ipAddress[16];
+		// 알아보기쉽게 문자열로 변환
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+		cout << "Client Connected! IP = " << ipAddress << endl;
 
 
+		while (true)
+		{
+			SOCKADDR_IN clientAddr; // IPv4
+			::memset(&clientAddr, 0, sizeof(clientAddr));
+			int32 addrLen = sizeof(clientAddr);
 
-	
+			SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+			if (clientSocket == INVALID_SOCKET)
+			{
+				int32 errCode = ::WSAGetLastError();
+				cout << "Accept ErrorCode : " << errCode << endl;
+				return 0;
+			}
+
+			// 손님 입장!
+			char ipAddress[16];
+			::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+			cout << "Client Connected! IP = " << ipAddress << endl;
+
+			// TODO
+			while (true)
+			{
+				char recvBuffer[1000];
+
+				this_thread::sleep_for(1s);
+
+				int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+				if (recvLen <= 0)
+				{
+					int32 errCode = ::WSAGetLastError();
+					cout << "Recv ErrorCode : " << errCode << endl;
+					return 0;
+				}
+
+				cout << "Recv Data! Data = " << recvBuffer << endl;
+				cout << "Recv Data! Len = " << recvLen << endl;
+
+				/*int32 resultCode = ::send(clientSocket, recvBuffer, recvLen, 0);
+				if (resultCode == SOCKET_ERROR)
+				{
+					int32 errCode = ::WSAGetLastError();
+					cout << "Send ErrorCode : " << errCode << endl;
+					return 0;
+				}*/
+			}
+		}
+	}
+
+	// -----------------------------
+
+
+	// 윈속 종료
+	::WSACleanup();
 }
